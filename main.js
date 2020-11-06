531a6fb4239fcd3859818b1ae3d0fda58d4cd202
dnodolf1212 <dnodolf@gmail.com>
2020-11-05 22:41:20 -0800

    Done.

--- main.js
+++ main.js
@@ -1,17 +1,46 @@
 // Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+const EMPTY_HEART = '♡';
+const FULL_HEART = '♥';
+let artLikes = document.querySelectorAll('.like-glyph');
 
-// Your JavaScript code goes here!
+function likeHappens(e){ 
+  let heart = e.target;
+  heart.innerText = EMPTY_HEART;
+  mimicServerCall()
+    .then( function(){
+      heart.innerHTML = FULL_HEART;
+      heart.className = 'activated-heart'
+    })//end of the then
+    .catch( function(reject){
+      let myDiv = document.getElementById('modal');
+      let divText = document.getElementById('modal-message');
+      myDiv.classList.value = "";
+      divText.innerText = reject
+      setTimeout(function() {
+        myDiv.classList.value = "hidden";
+      }, 5000); 
+    });//end of the catch
+}//end of like happens
+      
 
-
+  for (let h of artLikes) {
+    h.addEventListener('click', likeHappens)
+    if (h.innerText === FULL_HEART){
+      h.className = 'activated-heart'
+    } else {
+      h.innerText === EMPTY_HEART
+      h.className = ''
+    }
+  }
+  
+  
 
 
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
 //------------------------------------------------------------------------------
 
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
+function mimicServerCall() {
   return new Promise(function(resolve, reject) {
     setTimeout(function() {
       let isRandomFailure = Math.random() < .2
